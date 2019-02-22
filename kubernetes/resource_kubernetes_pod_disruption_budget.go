04f6000117ff64ae6a29cc0216cff28536c32281
Jethro Carr <jcarr@sailthru.com>
2019-02-22 13:44:19 +1300

    Add support for Kubernetes Pod Disruption Budget
    This resource includes validation to ensure PDB is correctly set to either
    min_avaliable or max_unavailable and supports both integer and percentages.
    
    As this resource cannot be updated in Kubernetes, we only ever create and
    delete upon changes.

---
+++ kubernetes/resource_kubernetes_pod_disruption_budget.go
@@ -0,0 +1,133 @@
+package kubernetes
+
+import (
+	"log"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func resourceKubernetesPodDisruptionBudget() *schema.Resource {
+
+	return &schema.Resource{
+		Create: resourceKubernetesPodDisruptionBudgetCreate,
+		Read:   resourceKubernetesPodDisruptionBudgetRead,
+		Delete: resourceKubernetesPodDisruptionBudgetDelete,
+
+		Schema: map[string]*schema.Schema{
+			"metadata": namespacedMetadataSchema("pod disruption budget", true),
+			"spec": {
+				Type:        schema.TypeList,
+				Description: "Spec defines the specification of the Pod Disruption Budget.`",
+				Required:    true,
+				MaxItems:    1,
+				ForceNew:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"min_available": {
+							Type:        schema.TypeString,
+							Description: "Minimum amount of pods that need to stay available. You can set it to 100% to prevent all voluntary evictions. This is a mutually exclusive setting with MaxUnavailable.",
+							Optional:    true,
+							ForceNew:    true,
+						},
+						"selector": {
+							Type:        schema.TypeMap,
+							Description: "A label query over pods that should match the Pod Disruption Budget.",
+							Required:    true,
+							ForceNew:    true,
+						},
+						"max_unavailable": {
+							Type:        schema.TypeString,
+							Description: "This is the number pf Pods that should be unavailable after an eviction. Setting this to 0 would prevent any voluntary evictions. This is a mutually exclusive setting with MinAvailable. Only available in Kubernetes 1.7 and higher.",
+							Optional:    true,
+							ForceNew:    true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceKubernetesPodDisruptionBudgetCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*kubernetesProvider).conn
+	metadata := expandMetadata(d.Get("metadata").([]interface{}))
+	spec, err := expandPodDisruptionBudgetSpec(d.Get("spec").([]interface{}))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Before Creating pod disruption budget: %#v", d)
+
+	podDisruptionBudget := v1beta1.PodDisruptionBudget{
+		ObjectMeta: metadata,
+		Spec:       spec,
+	}
+	log.Printf("[INFO] Creating pod disruption budget: %#v", podDisruptionBudget)
+
+	createdPDB, err := conn.PolicyV1beta1().PodDisruptionBudgets(metadata.Namespace).Create(&podDisruptionBudget)
+	if err != nil {
+		return err
+	}
+	log.Printf("[INFO] Submitted new pod disruption budget: %#v", createdPDB)
+	d.SetId(buildId(createdPDB.ObjectMeta))
+
+	return resourceKubernetesPodDisruptionBudgetRead(d, meta)
+}
+
+func resourceKubernetesPodDisruptionBudgetRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*kubernetesProvider).conn
+
+	namespace, name, err := idParts(d.Id())
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Reading pod disruption budget %s", name)
+	pdb, err := conn.PolicyV1beta1().PodDisruptionBudgets(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		log.Printf("[DEBUG] Received error: %#v", err)
+		return err
+	}
+	log.Printf("[INFO] Received pod disruption budget: %#v", pdb)
+
+	err = d.Set("metadata", flattenMetadata(pdb.ObjectMeta, d))
+	if err != nil {
+		return err
+	}
+
+	spec, err := flattenPodDisruptionBudgetSpec(pdb.Spec)
+	if err != nil {
+		return err
+	}
+
+	err = d.Set("spec", spec)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceKubernetesPodDisruptionBudgetDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*kubernetesProvider).conn
+
+	namespace, name, err := idParts(d.Id())
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Deleting pod disruption budget: %#v", name)
+	err = conn.PolicyV1beta1().PodDisruptionBudgets(namespace).Delete(name, &metav1.DeleteOptions{})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Pod disruption budget %s deleted", name)
+
+	// d.SetId("") is automatically called assuming delete returns no errors, but
+	// it is added here for explicitness.
+	d.SetId("")
+	return nil
+}
